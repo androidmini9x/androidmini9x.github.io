ae43a0b5f7ce8c4a126ae4bec0c5886fab4baf55
atck3r <atck3r@localdomain>
2024-01-16 18:35:43 +0300

    Imporve excerpt post

--- .eleventy.js
+++ .eleventy.js
@@ -119,7 +119,21 @@ function extractExcerpt(article) {
 
     const content = article.templateContent;
 
-    const excerpt = content.slice(0, content.indexOf("\n"));
+    const separatorsList = [
+        { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+        { start: '<p>', end: '</p>' }
+    ];
+
+
+    separatorsList.some(separators => {
+        const startPosition = content.indexOf(separators.start);
+        const endPosition = content.indexOf(separators.end);
+    
+        if (startPosition !== -1 && endPosition !== -1) {
+            excerpt = content.substring(startPosition + separators.start.length, endPosition).trim() + ".....";
+            return true; // Exit out of array loop on first match
+        }
+    });
 
     return excerpt;
 }
