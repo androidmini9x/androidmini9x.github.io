f1daf92c0405073424b02d8352594553ae07ebbd
atck3r <androidmini92@gmail.com>
2025-08-11 22:16:37 +0300

    Add Data

---
+++ src/_data/cert.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        name: "AWS Certified Cloud Practitioner",
+        by: "Amazon",
+        date: "August 2024"
+    },
+    {
+        name: "GitHub Foundations",
+        by: "GitHub",
+        date: "August 2024"
+    },
+    {
+        name: "Google Cloud Associate Cloud Engineer",
+        by: "Google",
+        date: "June 2024"
+    },
+    {
+        name: "React Web Development",
+        by: "Flatiron/Re:Coded",
+        date: "May 2022"
+    },
+    {
+        name: "Oracle Certified Associate, Java SE 8 Programmer",
+        by: "Google",
+        date: "April 2018"
+    }
+];
---
+++ src/_data/skill.js
@@ -0,0 +1,55 @@
+module.exports = {
+    skills: [
+        {
+            name: "Programming Languages",
+            items: [
+                {name: "Java", img: "Java.svg"},
+                {name: "C/C++", img: "C++ (CPlusPlus).svg"},
+                {name: "Javascript", img: "JavaScript.svg"},
+                {name: "Typescript", img: "TypeScript.svg"},
+                {name: "Python", img: "Python.svg"},
+                {name: "HTML", img: "HTML5.svg"},
+                {name: "CSS", img: "CSS3.svg"},
+                {name: "PHP", img: "PHP.svg"},
+                {name: "SQL", img: "SQL Developer.svg"}
+            ]
+        },
+        {
+            name: "Frameworks & Libraries",
+            items: [
+                {name: "JAVA EE/Spring", img: "Spring.svg"},
+                {name: "React.js/React Native", img: "React.svg"},
+                {name: "Next.js", img: "Next.js.svg"},
+                {name: "Redux", img: "Redux.svg"},
+                {name: "jQuery", img: "jQuery.svg"},
+                {name: "Flask", img: "Flask.svg"},
+                {name: "Express.js", img: "Express.svg"},
+                {name: "Laravel", img: "Laravel.svg"},
+                {name: "Bootstrap", img: "Bootstrap.svg"},
+                {name: "Tailwind CSS", img: "Tailwind CSS.svg"},
+            ]
+        },
+        {
+            name: "Technologies and Tools",
+            items: [
+                {name: "Node.js", img: "Node.js.svg"},
+                {name: "Linux", img: "Linux.svg"},
+                {name: "Nginx", img: "NGINX.svg"},
+                {name: "Firebase", img: "Firebase.svg"},
+                {name: "MySQL", img: "MySQL.svg"},
+                {name: "PostgresSQL", img: "PostgresSQL.svg"},
+                {name: "Git/GitHub", img: "GitHub.svg"},
+                {name: "Material-UI", img: "Material UI.svg"},
+                {name: "Maven", img: "Apache Maven.svg"},
+                {name: "Webpack", img: "Webpack.svg"},
+                {name: "AWS (Amazon Web Services)", img: "AWS.svg"},
+                {name: "Azure", img: "Azure.svg"},
+                {name: "Google Cloud", img: "Google Cloud.svg"},
+                {name: "JUnit", img: "JUnit.svg"},
+                {name: "Jest", img: "Jest.svg"},
+                {name: "Jira", img: "Jira.svg"},
+                {name: "Trello", img: "Trello.svg"}
+            ]
+        }
+    ]
+}
